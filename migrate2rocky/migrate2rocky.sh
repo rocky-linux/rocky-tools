b587a7e7a46ce4b53fdbc251150b1b029788dd29
Peter Ajamian <peter@pajamian.dhs.org>
2021-06-13 00:30:05 +1200

    Move more tests to the top
    Test for teh bash version and root at the top as well, to avoid errors from
    commands that otherwise get run before the tests.

--- migrate2rocky/migrate2rocky.sh
+++ migrate2rocky/migrate2rocky.sh
@@ -32,11 +32,21 @@
 ## Rocky is RC status. Using this script means you accept all risks of system
 ## instability.
 
-# This check needs to be right at the top because we start with bash-isms right
+# These checks need to be right at the top because we start with bash-isms right
 # away in this script.
-# We need bash version >= 4 for associative arrays.  This will also verify
-# that we're actually running bash.
 if [ -n "$POSIXLY_CORRECT" ] || [ -z "$BASH_VERSION" ]; then
+    printf '%s\n' "bash >= 4.0 is required for this script." >&2
+    exit 1
+fi
+
+# Make sure we're root.
+if (( EUID != 0 )); then
+    printf '%s\n' "You must run this script as root.  Either use sudo or 'su -c ${0}'" >&2
+    exit 1
+fi
+
+# We need bash version >= 4 for associative arrays.
+if (( BASH_VERSINFO < 4 )); then
     printf '%s\n' "bash >= 4.0 is required for this script." >&2
     exit 1
 fi
@@ -124,17 +134,6 @@ pre_check () {
 # work unless it's severly broken.  This is just a simple check that will cause
 # the script to bail if any expected system utilities are missing.
 bin_check() {
-    # Make sure we're root.
-    if (( EUID != 0 )); then
-	exit_message "You must run this script as root.  Either use sudo or 'su -c ${0}'"
-    fi
-
-    # We need bash version >= 4 for associative arrays.  This will also verify
-    # that we're actually running bash.
-    if (( BASH_VERSINFO < 4 )); then
-	exit_message "bash >= 4.0 is required for this script."
-    fi
-
     # Check the platform.
     if [[ $(os-release PLATFORM_ID) != "$SUPPORTED_PLATFORM" ]]; then
 	exit_message "This script must be run on an EL8 distribution.  Migration from other distributions is not supported."
