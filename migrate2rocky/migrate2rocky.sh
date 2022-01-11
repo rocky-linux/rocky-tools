2c155e9e4edd65b56024330106ca8f9d0207bb0f
Amin Vakil <info@aminvakil.com>
2022-01-11 09:30:13 +0330

    Revert "Ignore unused variable shellcheck warning"
    This reverts commit 18ac2f69c5a6c69925e2e7d330d557f877d70b46.

--- migrate2rocky/migrate2rocky.sh
+++ migrate2rocky/migrate2rocky.sh
@@ -283,7 +283,6 @@ pre_check () {
 
 	dir=${dirs[$((i++))]}
 
-        # shellcheck disable=SC2034
 	dir_mount_map[$dir]=$mount
 	mount_avail_map[$mount]=${avail%M}
 	(( mount_space_map[$mount]+=dir_space_map[$dir] ))
